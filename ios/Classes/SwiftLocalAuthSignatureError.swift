caf53d0db771e6606ae39455905384ebd72b8876
“prongbang” <prongbang@gmail.com>
2022-12-25 15:23:23 +0700

    Update error code for ios

---
+++ ios/Classes/SwiftLocalAuthSignatureError.swift
@@ -0,0 +1,23 @@
+//
+//  SwiftLocalAuthSignatureError.swift
+//  local_auth_signature
+//
+//  Created by M on 25/12/2565 BE.
+//
+
+import Foundation
+
+class SwiftLocalAuthSignatureError {
+    static let ArgsIsNull = "ArgsIsNull"
+    static let ReasonIsNull = "ReasonIsNull"
+    static let KeyIsNull = "KeyIsNull"
+    static let PayloadIsNull = "PayloadIsNull"
+    static let SignatureIsNull = "SignatureIsNull"
+    static let NoFragmentActivity = "NoFragmentActivity"
+    static let PasscodeNotSet = "PasscodeNotSet"
+    static let NotAvailable = "NotAvailable"
+    static let LockedOut = "LockedOut"
+    static let PermanentlyLockedOut = "PermanentlyLockedOut"
+    static let Canceled = "Canceled"
+    static let Error = "Error"
+}
--- ios/Classes/SwiftLocalAuthSignaturePlugin.swift
+++ ios/Classes/SwiftLocalAuthSignaturePlugin.swift
@@ -14,52 +14,94 @@ public class SwiftLocalAuthSignaturePlugin: NSObject, FlutterPlugin {
     
     public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
         guard let args = call.arguments as? Dictionary<String, String> else {
-            result(nil)
+            result(
+                FlutterError(
+                    code: SwiftLocalAuthSignatureError.ArgsIsNull,
+                    message: "Arguments is null",
+                    details: nil
+                )
+            )
             return
         }
         
         guard let key = args[SwiftLocalAuthSignatureArgs.Key] else {
-            result(nil)
+            result(
+                FlutterError(
+                    code: SwiftLocalAuthSignatureError.KeyIsNull,
+                    message: "Key is null",
+                    details: nil
+                )
+            )
             return
         }
         
         switch call.method {
         case SwiftLocalAuthSignatureMethod.CreateKeyPair:
             guard let reason = args[SwiftLocalAuthSignatureArgs.Reason] else {
-                result(nil)
+                result(
+                    FlutterError(
+                        code: SwiftLocalAuthSignatureError.ReasonIsNull,
+                        message: "Reason is null",
+                        details: nil
+                    )
+                )
                 return
             }
             
             let keyConfig = KeyConfig(name: key)
             let signatureBiometricManager = LocalSignatureBiometricManager.newInstance(keyConfig: keyConfig)
             signatureBiometricManager.createKeyPair(reason: reason) { value in
-                result(value)
+                if value.status == "success" {
+                    result(value.publicKey)
+                } else {
+                    result(
+                        FlutterError(
+                            code: value.status,
+                            message: "Error is \(value.status)",
+                            details: nil
+                        )
+                    )
+                }
             }
-          
+            
             break
         case SwiftLocalAuthSignatureMethod.Sign:
             guard let payload = args[SwiftLocalAuthSignatureArgs.Payload] else {
-                result(nil)
+                result(
+                    FlutterError(
+                        code: SwiftLocalAuthSignatureError.PayloadIsNull,
+                        message: "Payload is null",
+                        details: nil
+                    )
+                )
                 return
             }
             
             let keyConfig = KeyConfig(name: key)
             let signatureBiometricManager = LocalSignatureBiometricManager.newInstance(keyConfig: keyConfig)
             signatureBiometricManager.sign(payload: payload) { signature in
-                if signature != nil {
-                    result(SignResult(payload: payload, signature: signature!))
-                } else {
-                    result(nil)
-                }
+                result(signature)
             }
             break
         case SwiftLocalAuthSignatureMethod.Verify:
             guard let payload = args[SwiftLocalAuthSignatureArgs.Payload] else {
-                result(nil)
+                result(
+                    FlutterError(
+                        code: SwiftLocalAuthSignatureError.PayloadIsNull,
+                        message: "Payload is null",
+                        details: nil
+                    )
+                )
                 return
             }
             guard let signature = args[SwiftLocalAuthSignatureArgs.Signature] else {
-                result(nil)
+                result(
+                    FlutterError(
+                        code: SwiftLocalAuthSignatureError.SignatureIsNull,
+                        message: "Signature is null",
+                        details: nil
+                    )
+                )
                 return
             }
             
@@ -72,7 +114,7 @@ public class SwiftLocalAuthSignaturePlugin: NSObject, FlutterPlugin {
         default:
             result(nil)
         }
-       
+        
     }
     
 }
